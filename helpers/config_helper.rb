4c84c74c5f7ac03aa10eddf15255e1e3d0f4f0da
James Stradling <jt.stradling@gmail.com>
2010-01-08 18:15:11 +1300

    Updated ConfigHelpers to allow the limiting of recursive traversal when finding files; Fixed a couple of misc bugs.

--- helpers/config_helper.rb
+++ helpers/config_helper.rb
@@ -3,34 +3,48 @@ module ConfigHelper
   
   protected
   
-    ## Returns an Array of .rb file names in the given directory.
-    def get_files_in(directory_name)
-      
-      ## If directory name is not absolute, go one-up to what should
-      ## be the root directory of the app. (I.e. We're in ROOT/helpers.)
-      path = directory_name =~ /^\// ? directory_name : File.join(File.dirname(__FILE__), '..', directory_name)
+  ## Returns an Array of .rb file names in the given directory.
+  ## If the option :traverse => true is set, recurses into
+  ## sub-directories to load files.
+  def get_files_in(directory_name, options = {})
+    options = {
+      :traverse => false,
+      :full_paths => true
+    }.merge(options)
     
-      ## Only select .rb files
-      Dir["#{path}/**/*.rb"].select { |e| e =~ /^.+\.rb$/ }
-    end
+    ## If directory name is not absolute, go one-up to what should
+    ## be the root directory of the app. (I.e. We're in ROOT/helpers.)
+    path = directory_name =~ /^\// ? directory_name : File.join(File.dirname(__FILE__), '..', directory_name)
     
-    ## Converts an Array of file names to an Array of camel-cased
-    ## class names based on the file names in the given directory.
-    def get_class_names_in(directory_name)
-      tableized_names = get_files_in(directory_name).map { |n| n.subtract(".rb") }
-      tableized_names.map { |names| names.camelize }
-    end
+    pattern = options[:traverse] ? "/**/*.rb" : "/*.rb"
+    files = Dir[path + pattern].select { |e| e =~ /^.+\.rb$/ }
 
-    ## Loads files in the given directory. The directory is assumed to
-    ## be relative to the app root (I.e. ../).
-    def load_files_in(directory_name)
-      get_files_in(directory_name).sort.each { |ext| load ext }
+    if options[:full_paths]
+      files
+    else
+      files.map { |file| file.match(/\/([A-Za-z0-9_]+\.rb)$/)[1] }
     end
+  end
+  
+  ## Converts an Array of file names to an Array of camel-cased
+  ## class names based on the file names in the given directory.
+  ## For options see #get_files_in.
+  def get_class_names_in(directory_name, options = {})
+    tableized_names = get_files_in(directory_name, { :full_paths => false }.merge(options)).map { |n| n.subtract(".rb") }
+    tableized_names.map { |names| names.camelize }
+  end
 
-    ## Initializes DataMapper connection to MySQL
-    def setup_datamapper
-      require 'dm-core'
-      require 'dm-timestamps'
-      DataMapper.setup(:default, 'mysql://localhost/sinatra-starter-kit')
-    end
+  ## Loads files in the given directory. The directory is assumed to
+  ## be relative to the app root (I.e. ../).
+  ## For options see #get_files_in.
+  def load_files_in(directory_name, options = {})
+    get_files_in(directory_name, options).sort.each { |ext| load ext }
+  end
+
+  ## Initializes DataMapper connection to MySQL
+  def setup_datamapper
+    require 'dm-core'
+    require 'dm-timestamps'
+    DataMapper.setup(:default, 'mysql://localhost/sinatra-starter-kit')
+  end
 end
